ee91dbd618c5a0643fa90114b1c2919bdedab701
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-05 22:07:55 +0330

    Change names

--- src/controllers/userController.ts
+++
@@ -1,6 +0,0 @@
-import {Request , Response} from 'express';
-
-export function me(req:Request,res:Response){
-    res.send("user profile");
-}
-
--- src/routes/user.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { me } from "../controllers/userController";
-const router = Router();
-
-
-router.get('/me' , me);
-
-
-export default router ; 
