293759263369bb988866b16ea0b216fa51b64004
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-05 22:05:44 +0330

    Add main route

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,35 @@
-import express, { Application } from 'express';
+import {config} from 'dotenv';
+config()
+
+import './auth/jwt/jwt.strategy';
+
+import express from 'express';
 import helmet from 'helmet' ;
 import morgan from 'morgan';
-import logger from './utils/logger/logger';
+import logger from './shared/logger/logger';
+import passport from 'passport';
+import mainRoutes from './routes';
+import { databaseConnection } from './config/database.config';
+
 
 (()=>{
     const app = express()
     const port = process.env.PORT || 3001 ; 
     
-    app.use(express.static('public'))
-    app.use(express.urlencoded({extended : true}))
-    app.use(express.json())
-    app.use(helmet())
-    app.use(morgan("tiny"))
-
-    app.get('/' , (req,res)=>{
-        res.send('hello world')
-    })
+    app.use(express.static('public'));
+    app.use(express.urlencoded({extended : true}));
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan("tiny"));
+    app.use(passport.initialize());
+    
+    // all routes
+    app.use(mainRoutes);
 
     app.listen(port , ()=>{
         console.log(process.version)
+        databaseConnection()
         console.log(`app running on port ${port}`)
         logger.info(`app running on port ${port}`)
     })
 })()
-
