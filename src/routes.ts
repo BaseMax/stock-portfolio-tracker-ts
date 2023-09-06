a5d1ebec9157afb339d02353bbd900721edffdbe
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-06 23:23:54 +0330

    Add other routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import userRouter from './user/user.route';
 import authRouter from './auth/auth.route';
+import protofiloRouter from './protofilo/protofilo.route';
+import stockRouter from './stock/stock.route';
 const router = Router()
 
 router.use('/user' , userRouter);
 router.use('/auth',authRouter);
-
+router.use('/protofilo',protofiloRouter);
+router.use('/stock' , stockRouter);
 
 export default router ; 
