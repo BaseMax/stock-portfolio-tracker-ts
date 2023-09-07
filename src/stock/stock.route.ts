15b9293d00b6b9f596e5c6b13fb6a18451fb0052
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-07 21:31:38 +0330

    Add new route

--- src/stock/stock.route.ts
+++ src/stock/stock.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createStock, findStock } from "./stock.controller";
+import { createStock, findStock, findStockById, findStockBySymbol, removeStock } from "./stock.controller";
 import passport from "passport";
 import { createStockValidation } from "./validation/create-stock.validation";
 import { validation } from "../shared/errors/validation";
@@ -11,8 +11,32 @@ router.post(
     passport.authenticate('jwt',{session:false}) ,
     createStockValidation() , 
     validation , 
-    createStock);
-router.get('/' , passport.authenticate('jwt',{session:false}) ,findStock);
+    createStock
+);
 
+router.get(
+    '/' , 
+    passport.authenticate('jwt',{session:false}) ,
+    findStock
+);
+
+router.get(
+    '/:id' , 
+    passport.authenticate('jwt',{session:false}) ,
+    findStockById ,
+);
+
+router.get(
+    '/symbol/:symbol' , 
+    passport.authenticate('jwt',{session:false}) ,
+    findStockBySymbol,
+);
+
+
+router.delete(
+    '/:id' , 
+    passport.authenticate('jwt',{session:false}) , 
+    removeStock
+)
 
 export default router ; 
