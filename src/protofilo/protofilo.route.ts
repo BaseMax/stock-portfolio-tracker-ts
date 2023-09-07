795b07ac942e1f79485a69211f2169fd0db0600c
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-07 21:30:54 +0330

    Add new route

--- src/protofilo/protofilo.route.ts
+++ src/protofilo/protofilo.route.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { addStockToProtofilo, createProtofilo ,findAllProtofilo, removeStockToProtofilo } from "./protofilo.controller";
+import { 
+    addStockToProtofilo, 
+    createProtofilo ,
+    findAllProtofilo, 
+    findOneProtofilo, 
+    removeProtofilo, 
+    removeStockToProtofilo, 
+    updateProtofilo 
+} from "./protofilo.controller";
 import passport from "passport";
 import { createProtofiloValidation } from "./validation/create-protofilo.validation";
 import { validation } from "../shared/errors/validation";
 import { addStockValidation } from "./validation/add-stock.validation";
+import { updateProtofiloValidation } from "./validation/update-protofilo.validation";
 
 const router = Router();
 
@@ -16,10 +25,32 @@ router.post(
 )
 
 
+router.put(
+    '/:id/' , 
+    passport.authenticate('jwt' , {session:false}),
+    updateProtofiloValidation(),
+    validation , 
+    updateProtofilo ,
+)
+
+
+router.delete(
+    '/:id/' , 
+    passport.authenticate('jwt' , {session:false}),
+    removeProtofilo ,
+)
+
+
 router.get(
     '/' ,
     passport.authenticate('jwt' , {session:false}),
     findAllProtofilo ,
+)
+
+router.get(
+    '/:id' ,
+    passport.authenticate('jwt' , {session:false}),
+    findOneProtofilo ,
 )
 
 
