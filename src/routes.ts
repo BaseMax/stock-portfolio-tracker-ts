c08cad208e386d64e12122613c3587247260c686
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-08 10:23:47 +0330

    Add new route

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,13 @@ import userRouter from './user/user.route';
 import authRouter from './auth/auth.route';
 import protofiloRouter from './protofilo/protofilo.route';
 import stockRouter from './stock/stock.route';
+import settingRouter from './setting/setting.route';
 const router = Router()
 
 router.use('/user' , userRouter);
 router.use('/auth',authRouter);
 router.use('/protofilo',protofiloRouter);
 router.use('/stock' , stockRouter);
+router.use('/setting' , settingRouter);
 
 export default router ; 
