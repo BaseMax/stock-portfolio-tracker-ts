6620fd64f2ff1c4b0914d38f04f595237ad3894b
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-09 11:56:34 +0330

    Add transaction code

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import authRouter from './auth/auth.route';
 import protofiloRouter from './protofilo/protofilo.route';
 import stockRouter from './stock/stock.route';
 import settingRouter from './setting/setting.route';
+import transactionRouter from './transaction/transaction.route';
 const router = Router()
 
 router.use('/user' , userRouter);
@@ -11,5 +12,7 @@ router.use('/auth',authRouter);
 router.use('/protofilo',protofiloRouter);
 router.use('/stock' , stockRouter);
 router.use('/setting' , settingRouter);
+router.use('/transaction' ,transactionRouter)
+
 
 export default router ; 
