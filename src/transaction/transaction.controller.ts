fd574ed98cca8128d9a95892db6a5c738d44ab70
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-09 11:58:03 +0330

    End module

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,33 +1,153 @@
 import { Request , Response } from 'express';
+import { Stock } from '../stock/models/stock.model';
+import { Transaction } from './models/transaction.model';
+import { IUser } from '../user/user.interface';
+import { TransactionStatusEnum } from './enums/transaction-status.enum';
 
-// const createTransaction =async (req:Request,res:Response) => {
+const createTransaction =async (req:Request,res:Response) => {
+    const user = req.user as  IUser ; 
+    const stockId = req.params.stockId ;
+    const {
+        quantity , 
+        type , 
+    } = req.body ;
 
-// }
+    const stock = await Stock.findOne({_id:stockId});
+
+    if(!stock){
+        return res.status(404).json({
+            message : 'Stock not found' ,
+            success : false ,
+        })
+    }
+
+    const amount = quantity * stock.price ; 
+
+    const newTransaction = await Transaction.create({
+        type , 
+        amount , 
+        quantity ,
+        user : user.id , 
+        stock : stock.id , 
+        status : TransactionStatusEnum.PENDING ,
+    })
+
+    const result = await newTransaction.save()
+
+    res.status(201).json({
+        message : 'Item created successfully',
+        success:true,
+        id:result.id ,
+    })
+}
 
 
-const updateTransaction = async (req:Request,res:Response) => {
+const buyTransaction = async (req:Request,res:Response) => {
+    // This controller is the face for change data 
+    // if you want use payment gateway make this controller 
 
+    const user = req.user as IUser ; 
+    const id = req.params.id ;
+
+
+    const query = {
+        _id : id , 
+        user : user.id ,
+    }
+
+    const transaction = await Transaction.findOne(query)
+        .populate('user')
+        .populate('stock')
+        .exec()
+
+    
+    if(!transaction){
+        return res.status(404)
+            .json({
+                message : 'Transaction not found' ,
+                success : false,
+            })
+    }
+
+
+    transaction.status = TransactionStatusEnum.SUCCESS ;
+    await transaction.save();
+
+    res.send({
+        message : 'success',
+    })
 }
 
 
 const removeTransaction = async (req:Request,res:Response) => {
+    const user = req.user as IUser ; 
+    const id = req.params.id ;
+
+
+    const query = {
+        _id : id , 
+        user : user.id ,
+    }
+
+    const transaction = await Transaction.findOne(query)
+        .populate('user')
+        .populate('stock')
+        .exec()
+
     
+    if(!transaction){
+        return res.status(404)
+            .json({
+                message : 'Transaction not found' ,
+                success : false,
+            })
+    }
+
+
+    await Transaction.deleteOne(query);
+    
+    res.send({
+        message : 'Transaction removed successfully',
+        success: true
+    })
 }
 
 
 const findOneTransaction = async (req:Request,res:Response) => {
-    
+    const user = req.user as IUser ; 
+    const id = req.params.id ;
+
+
+    const query = {
+        _id : id , 
+        user : user.id ,
+    }
+
+    const transaction = await Transaction.findOne(query)
+        .populate('user')
+        .populate('stock')
+        .exec()
+
+
+    res.send(transaction);
 }
 
 
 const findAllTransaction = async (req:Request,res:Response) => {
-    
+    const user = req.user as IUser ; 
+    const transactions = await Transaction.find({user : user.id })
+        .populate('user')
+        .populate('stock')
+        .exec()
+
+    res.send(transactions)
 }
 
 
 export {  
+    createTransaction , 
     removeTransaction , 
-    updateTransaction ,
+    buyTransaction ,
     findOneTransaction , 
     findAllTransaction
 }
--- src/transaction/transaction.route.ts
+++ src/transaction/transaction.route.ts
@@ -1,6 +1,53 @@
 import { Router } from "express";
+import passport from "passport";
+import { createTransactionValidator } from "./validator/transaction.validator";
+import { validation } from "../shared/errors/validation";
+import { 
+    buyTransaction, 
+    createTransaction, 
+    findAllTransaction, 
+    findOneTransaction, 
+    removeTransaction
+} from "./transaction.controller";
 
 const router = Router();
+
+router.post(
+    '/:stockId',
+    passport.authenticate('jwt' , {session:false}),
+    createTransactionValidator() , 
+    validation , 
+    createTransaction
+)
+
+
+
+router.put(
+    '/:id/buy',
+    passport.authenticate('jwt' , {session:false}), 
+    buyTransaction , 
+)
+
+
+router.get(
+    '/:id',
+    passport.authenticate('jwt' , {session:false}), 
+    findOneTransaction , 
+)
+
+
+router.get(
+    '/',
+    passport.authenticate('jwt' , {session:false}), 
+    findAllTransaction ,
+)
+
+
+router.delete(
+    '/:id',
+    passport.authenticate('jwt' , {session:false}), 
+    removeTransaction ,
+)
 
 
 
